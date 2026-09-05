779b245b335a629cd394e2c422c816f59c3619a9
4A-22\koyo <a>
2026-09-05 11:45:08 +0900

    CircleMove.h　を追加 GameScene.h　SceneBase　をオーバーライド GameScene.cpp　に各種　関数を追加

---
+++ GameScene.cpp
@@ -0,0 +1,25 @@
+#include "GameScene.h"
+
+void GameScene::Init()
+{
+	// ‰Šú‰»ˆ—
+}
+void GameScene::Input()
+{
+	// “ü—Íˆ—
+}
+
+void GameScene::Update()
+{
+	// XVˆ—
+}
+
+void GameScene::Draw()
+{
+	// •`‰æˆ—
+}
+
+void GameScene::Sound_play()
+{
+	// ‰¹ºÄ¶ˆ—
+}
--- GameScene.h
+++ GameScene.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "SceneBase.h"
+class GameScene : public SceneBase
+{
+public:
+	GameScene() {}
+
+	void Init() override {}
+	void Input() override {}
+	void Update() override {}
+	void Draw() override {}
+	void Sound_play() override {}
+};
