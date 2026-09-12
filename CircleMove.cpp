bda4d899758c2a4c667917414f4ad96bf3907ea9
4A-22\koyo <a>
2026-09-12 11:38:58 +0900

    画像読み込み

--- CircleMove.cpp
+++ CircleMove.cpp
@@ -2,8 +2,8 @@
 
 void CircleMove::Init()
 {
-	o_circle.image = LoadGraph("o.png");
-	g_circle.image = LoadGraph("g.png");
+	o_circle.image = LoadGraph("o_maru.png");
+	g_circle.image = LoadGraph("g_maru.png");
 
 }
 
@@ -19,8 +19,8 @@ void CircleMove::Update()
 
 void CircleMove::Draw()
 {
-		DrawGraph(g_circle.x, g_circle.y, g_circle.image, true);
-				DrawGraph(o_circle.x, o_circle.y, o_circle.image, true);
+	DrawGraph(g_circle.x, g_circle.y, g_circle.image, true);
+	DrawGraph(o_circle.x, o_circle.y, o_circle.image, true);
 
 }
 
