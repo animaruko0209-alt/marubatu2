295e285ee1f4fb3b6d2e2a64efcef4ea5134aeb0
yuuse_50wm <animaruko0209@gmail.com>
2026-09-12 23:28:21 +0900

    移動処理削除 o_circle　に座標を設定

--- CircleMove.cpp
+++ CircleMove.cpp
@@ -7,14 +7,13 @@ void CircleMove::Init()
 
 	g_circle.x = 175;
 	g_circle.y = 175;
+	o_circle.x = 395;
+	o_circle.y = 175;
 }
 
 void CircleMove::Input()
 {
-	if(CheckHitKey(KEY_INPUT_RIGHT))
-	{
-		g_circle.x += 50;
-	}
+
 
 }
 
@@ -26,7 +25,6 @@ void CircleMove::Draw()
 {
 	DrawGraph(o_circle.x, o_circle.y, o_circle.image, true);
 	DrawGraph(g_circle.x, g_circle.y, g_circle.image, true);
-
 }
 
 void CircleMove::Sound_play()
